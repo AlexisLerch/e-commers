449cad690c2d0510b493ca7e4bba5559671255e7
Alexis <alexis.lerch12@gmail.com>
2026-03-03 23:39:39 -0300

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,23 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma =
-  globalForPrisma.prisma ??
+  global.prisma ??
   new PrismaClient({
-    adapter,
+    log: ["query", "error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
